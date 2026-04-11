9534cbf68a0923e1f89db6821015c6aefe5d7aa4
The-code-of-Jon <jonathanh.poc@gmail.com>
2026-04-10 19:40:01 -0500

    Add files via upload

---
+++ clinic_querues(Step 2).sql
@@ -0,0 +1,40 @@
+USE clinic;
+
+SELECT SUM(Duration) AS Total_Appointment_Time_October
+FROM Appointment
+WHERE Date LIKE "2023-10-%";
+
+SELECT Patient_ID, Name, SSN, Date_Of_Birth, Home_Address, Insurance, Balance, Medical_Record_Number
+FROM Patient
+WHERE Balance = (SELECT MAX(Balance) FROM Patient);
+
+SELECT Staff.Name, Staff_Emergency_Numbers.Emergency_Phone_Number
+FROM Staff_Emergency_Numbers
+INNER JOIN Staff
+on Staff_Emergency_Numbers.Staff_ID = Staff.Staff_ID
+WHERE Staff.NAME LIKE "Jerry%";
+
+SELECT Name, Home_Address, Preferred_Pharmacy_Address, Date, CONCAT(Physician_Notes, " ", Non_Physician_Notes) AS Notes, Medication_Prescribed
+FROM Medical_Record
+INNER JOIN MedicalRecord_Medications_Prescribed
+on Medical_Record.Record_ID = MedicalRecord_Medications_Prescribed.Record_ID
+INNER JOIN Patient
+on Medical_Record.Medical_Record_Number = Patient.Medical_Record_Number
+WHERE Medication_Prescribed = "Nitroglycerin";
+
+SELECT DISTINCT Insurance, COUNT(Patient_ID) AS Number_of_Patients
+FROM Patient
+GROUP BY Insurance
+ORDER BY Number_of_Patients DESC;
+
+SELECT Name, Date, Physician_Notes, Procedure_Name
+FROM Medical_Record
+INNER JOIN Medical_Record_Procedures
+on Medical_Record.Record_ID = Medical_Record_Procedures.Record_ID
+INNER JOIN Patient
+on Medical_Record.Medical_Record_Number = Patient.Medical_Record_Number
+WHERE Medical_Record_Procedures.Procedure_Name LIKE "%X-Ray";
+
+SELECT Position
+FROM Receptionist
+WHERE Staff_ID = (SELECT Staff_ID FROM Staff WHERE Name = "Pam Beesly");
